ab5975cfda9d19acc819f534b03d03bad53f6167
Muhammed Mortgy <m@mortgy.com>
2021-05-01 21:02:16 +0300

    create Publisher base (Pub Sub)

---
+++ Challenge/Challenge/Shared/PubSub/Publisher.swift
@@ -0,0 +1,30 @@
+//
+//  Publisher.swift
+//  Challenge
+//
+//  Created by Mortgy on 5/1/21.
+//
+
+import Foundation
+
+class Publisher<T> {
+    private var subscribers: [((T?) -> Void)] = []
+    
+    var value: T? {
+        didSet {
+            subscribers.forEach {
+                $0(value)
+            }
+        }
+    }
+    
+    init(_ value: T?) {
+        self.value = value
+    }
+    
+    func bind(_ subscriber: @escaping (T?) -> Void) {
+        subscriber(value)
+        self.subscribers.append(subscriber)
+    }
+    
+}
